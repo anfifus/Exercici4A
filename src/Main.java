567f63dfc820d1029427217826c8ea7678c7b49e
anfifus <anfifus@gmail.com>
2022-01-30 14:52:50 +0100

    Cambio en la forma de tratar el bucle y añadido de la funcionalidad para mostrar tareas

--- src/Main.java
+++ src/Main.java
@@ -11,19 +11,22 @@ public class Main {
         System.out.println("3- Show all Task");
         System.out.println("4- Exit");
         Scanner scan = new Scanner(System.in);
-        int opMenu = 0;
+        int opMenu;
         ArrayList<Task> listTask = new ArrayList<>();
-        while(opMenu != 4)
+        do
         {
+            System.out.println("What option do you want to input?");
             opMenu = scan.nextInt();
             switch(opMenu)
             {
 
                 case 1-> addTask(listTask);
                 case 2-> dropByName(listTask);
-                case 3-> showAllTask();
+                case 3-> showAllTask(listTask);
+                case 4-> System.out.println("Exiting program...");
             }
         }
+        while(opMenu != 4);
     }
 
     private static void addTask(ArrayList<Task> listTask) {
@@ -32,7 +35,11 @@ public class Main {
         String nameTask = addTaskName();
         String descriptionTask = addTaskDescription();
         Task task = new Task(nameTask,descriptionTask);
-        listTask.add(task);
+        boolean isTaskAdded = listTask.add(task);
+        if(isTaskAdded)
+            System.out.println("The task was added successfully");
+        else
+            System.out.println("Task already exist");
     }
 
     private static String addTaskName() {
@@ -49,6 +56,7 @@ public class Main {
 //--------------------------------------------------------
     private static void dropByName(ArrayList<Task> listTask)
     {
+        System.out.println("Write the name of the task to be deleted");
         Scanner scan = new Scanner(System.in);
         String nameTask = scan.nextLine();
         int getIndex = 0;
@@ -59,12 +67,40 @@ public class Main {
             if(task.getName().equals(nameTask))
             {
                 listItTask.remove();
+                System.out.println("Task removed successfully");
             }
         }
 
     }
 
-    private static void showAllTask() {
+    private static void showAllTask(ArrayList<Task> listTask) {
+       /* ListIterator<Task> listItTask = listTask.listIterator();
+        while(listItTask.hasNext())
+        {
+            Task taskShow = listItTask.next();
+            System.out.println("The name of the task is: "+ taskShow.getName()+" and his description is: "+taskShow.getDescription());
+        } OLDForm*/
+        for(Task taskShow : listTask)
+        {
+            System.out.println("The name of the task is: "+ taskShow.getName()+" and his description is: "+taskShow.getDescription());
+        }
     }
 
 }
+
+
+/*Using while loop
+        int opMenu = 0;
+        ArrayList<Task> listTask = new ArrayList<>();
+        while(opMenu != 4)
+        {
+            opMenu = scan.nextInt();
+            switch(opMenu)
+            {
+
+                case 1-> addTask(listTask);
+                case 2-> dropByName(listTask);
+                case 3-> showAllTask();
+            }
+        }
+* */
